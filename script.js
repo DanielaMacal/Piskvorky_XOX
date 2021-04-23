9aa650d2322f36285fc9f9464029713e8fd6695e
Daniela Máčalová <Dan.Macalova@gmail.com>
2021-04-23 19:14:39 +0200

    4 FINAL

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// 'use strict';
-
-// let naTahu = 'circle';
-// const hrajeHrac = document.querySelector('.hraje-hrac');
-
-// document.querySelectorAll('.playBtn__btn').forEach((btn) => {
-//   btn.addEventListener('click', () => {
-//     if (naTahu === 'circle') {
-//       btn.classList.add('playBtn__btn--circle');
-//       hrajeHrac.src = 'img/circle.svg';
-//       naTahu = 'cross';
-//       button.target.disabled = true;
-//     } else {
-//       btn.classList.add('playBtn__btn--cross');
-//       hrajeHrac.src = 'img/cross.svg';
-//       naTahu = 'circle';
-//       button.target.disabled = true;
-//     }
-//   });
-// });
-
 'use strict';
 
 let naTahu = 'circle';
@@ -30,12 +9,10 @@ document.querySelectorAll('.playBtn__btn').forEach((btn) => {
       btn.classList.add('playBtn__btn--circle');
       hrajeHrac.src = 'img/circle.svg';
       naTahu = 'cross';
-      button.target.disabled = true;
     } else {
       btn.classList.add('playBtn__btn--cross');
       hrajeHrac.src = 'img/cross.svg';
       naTahu = 'circle';
-      button.target.disabled = true;
     }
   });
 });
